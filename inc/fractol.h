0123aed28d6b0c0e1e185755dbc24ba61412c205
Lucabhm <lbohm@student.42heilbronn.de>
2025-03-11 17:08:00 +0100

    try to implement zooming

--- inc/fractol.h
+++ inc/fractol.h
@@ -6,7 +6,7 @@
 /*   By: lbohm <lbohm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 16:14:42 by lucabohn          #+#    #+#             */
-/*   Updated: 2025/03/10 17:03:54 by lbohm            ###   ########.fr       */
+/*   Updated: 2025/03/11 16:54:11 by lbohm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,36 @@
 # include "../libs/ft_libft/libft.h"
 # include "../libs/ft_printf/ft_printf.h"
 # include "../libs/mlx/include/MLX42/MLX42.h"
+# include <stdio.h>
+# include <math.h>
 
 typedef struct s_data
 {
 	int			type;
-	mlx_t		*window_ptr;
+	int			win_width;
+	int			win_height;
+	mlx_t		*win_ptr;
 	mlx_image_t	*img_ptr;
+	float		offset;
+	float		zoom;
 }				t_data;
 
 typedef struct s_color
 {
-	char	r;
-	char	g;
-	char	b;
-	char	a;
+	int	r;
+	int	g;
+	int	b;
+	int	a;
 }				t_color;
 
 void		init_data(char *type, t_data *data);
+void		loop(void *param);
+void		resize(int width, int height, void *param);
+void		key(mlx_key_data_t keydata, void *param);
+void		scroll(double xdelta, double ydelta, void *param);
 void		create_fractal(t_data *data);
 void		calc_mandelbrot(float real, float imaginary, t_color *color);
-uint32_t	create_color(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
+uint32_t	create_color(t_color color);
 void		error(int msg, t_data *data);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,11 @@
 /*   By: lbohm <lbohm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 16:14:08 by lucabohn          #+#    #+#             */
-/*   Updated: 2025/03/10 17:01:50 by lbohm            ###   ########.fr       */
+/*   Updated: 2025/03/11 17:07:02 by lbohm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fractol.h"
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -20,9 +19,9 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 	{
 		init_data(argv[1], &data);
-		create_fractal(&data);
-		mlx_loop(data.window_ptr);
-		mlx_terminate(data.window_ptr);
+		mlx_loop_hook(data.win_ptr, loop, &data);
+		mlx_loop(data.win_ptr);
+		mlx_terminate(data.win_ptr);
 	}
 	else
 		error(1, NULL);
@@ -32,18 +31,68 @@ int	main(int argc, char **argv)
 void	init_data(char *type, t_data *data)
 {
 	if (!ft_strncmp(type, "Mandelbrot", ft_strlen(type)))
+	{
 		data->type = 0;
+		data->offset = 0.5;
+	}
 	else if (!ft_strncmp(type, "Julia", ft_strlen(type)))
 		data->type = 1;
 	else
 		error(2, NULL);
-	data->window_ptr = mlx_init(400, 400, "fractol", false);
-	if (!data->window_ptr)
+	data->win_width = 500;
+	data->win_height = 500;
+	data->win_ptr = mlx_init(data->win_width, data->win_height, "fractol", true);
+	if (!data->win_ptr)
 		error(3, NULL);
-	data->img_ptr = mlx_new_image(data->window_ptr, 400, 400);
+	data->img_ptr = mlx_new_image(data->win_ptr, data->win_width, data->win_height);
 	if (!data->img_ptr)
 		error(3, data);
-	mlx_image_to_window(data->window_ptr, data->img_ptr, 0, 0);
+	mlx_image_to_window(data->win_ptr, data->img_ptr, 0, 0);
+	data->zoom = 1.0;
+}
+
+void	loop(void *param)
+{
+	t_data	*data;
+
+	data = param;
+	mlx_resize_hook(data->win_ptr, resize, data);
+	mlx_key_hook(data->win_ptr, key, data);
+	mlx_scroll_hook(data->win_ptr, scroll, data);
+	create_fractal(data);
+	printf("time = %f\n", data->win_ptr->delta_time);
+}
+
+void	resize(int width, int height, void *param)
+{
+	t_data	*data;
+
+	data = param;
+	data->win_width = width;
+	data->win_height = height;
+	mlx_delete_image(data->win_ptr, data->img_ptr);
+	data->img_ptr = mlx_new_image(data->win_ptr, data->win_width, data->win_height);
+	if (!data->img_ptr)
+		error(3, data);
+	mlx_image_to_window(data->win_ptr, data->img_ptr, 0, 0);
+}
+
+void	key(mlx_key_data_t keydata, void *param)
+{
+	t_data	*data;
+
+	data = param;
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == 1)
+		error(0, data);
+}
+
+void	scroll(double xdelta, double ydelta, void *param)
+{
+	t_data	*data;
+
+	data = param;
+	printf("xdelta = %f ydelta = %f\n", xdelta, ydelta);
+	data->zoom += ydelta;
 }
 
 void	create_fractal(t_data *data)
@@ -55,34 +104,75 @@ void	create_fractal(t_data *data)
 	t_color	color = {0, 0, 0, 0};
 
 	y = 0;
-	while (y < 400)
+	while (y < data->win_height)
 	{
 		x = 0;
-		while (x < 400)
+		while (x < data->win_width)
 		{
-			real = -2.0 + ((float)x / 100.0);
-			imaginary = -2.0 + ((float)y / 100.0);
+			real = (-2.0 + (4 * (float)x / data->win_width) - data->offset) * data->zoom;
+			imaginary = (-2.0 + (4 * (float)y / data->win_height)) * data->zoom;
 			calc_mandelbrot(real, imaginary, &color);
-			mlx_put_pixel(data->img_ptr, x, y, create_color(252, 190, 17, 255));
+			mlx_put_pixel(data->img_ptr, x, y, create_color(color));
 			++x;
 		}
 		++y;
 	}
 }
 
-void	calc_mandelbrot(float real, float imaginary, t_color &color)
+void	calc_mandelbrot(float real, float imaginary, t_color *color)
 {
-	
+	float	it;
+	float	z_real;
+	float	z_imaginary;
+	float	z_real_tmp;
+	float	z_imaginary_tmp;
+
+	it = 0.0;
+	z_real = 0.0;
+	z_imaginary = 0.0;
+	while (it < 1000.0)
+	{
+		z_real_tmp = z_real * z_real - z_imaginary * z_imaginary + real;
+		z_imaginary_tmp = 2 * z_real * z_imaginary + imaginary;
+
+		z_real = z_real_tmp;
+		z_imaginary = z_imaginary_tmp;
+		if (sqrtf(z_real * z_real + z_imaginary * z_imaginary) > 2.0)
+			break ;
+		++it;
+	}
+	if (it == 1000.0)
+	{
+		color->r = 0;
+		color->g = 0;
+		color->b = 0;
+		color->a = 255;
+	}
+	else
+	{
+		float	diff = it / 1000.0;
+		color->r = 252 + diff * (50 - 252);
+		color->g = 190 + diff * (200 - 190);
+		color->b = 17 + diff * (100 - 17);
+		// color->r = 252;
+		// color->g = 190;
+		// color->b = 17;
+		// color->r = 50;
+		// color->g = 200;
+		// color->b = 100;
+		color->a = 255;
+	}
 }
 
-uint32_t	create_color(uint32_t r, uint32_t g, uint32_t b, uint32_t a)
+uint32_t	create_color(t_color color)
 {
-	return (r << 24 | g << 16 | b << 8 | a);
+	return (color.r << 24 | color.g << 16 | color.b << 8 | color.a);
 }
 
 void	error(int msg, t_data *data)
 {
-	ft_printf("Error: ");
+	if (msg != 0)
+		ft_printf("Error: ");
 	if (msg == 1)
 	{
 		ft_printf("wrong nbr of arguments\n\n");
@@ -94,6 +184,6 @@ void	error(int msg, t_data *data)
 	else if (msg == 3)
 		ft_printf("NULL ptr\n");
 	if (data)
-		mlx_terminate(data->window_ptr);
+		mlx_terminate(data->win_ptr);
 	exit(msg);
 }
